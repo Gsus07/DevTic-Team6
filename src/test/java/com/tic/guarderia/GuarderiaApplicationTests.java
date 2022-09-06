379ffb76f9998ef3f8697fc5c2180b3c39e47e97
Gsus07 <jesuscafo@15gmail.com>
2022-09-05 22:12:44 -0500

    first commit

---
+++ src/test/java/com/tic/guarderia/GuarderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tic.guarderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuarderiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
